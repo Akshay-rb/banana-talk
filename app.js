6e69bad9850538dfd8c777fdb20aa5fcfaa6ecce
Akshay-rb <akshayrbr2@gmail.com>
2021-07-28 10:16:23 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const btnTranslater = document.querySelector("#btn-translate")
 const txtInput = document.querySelector('#txt-input')
+const outputDiv = document.querySelector('#output')
 
-function clickEventHandler(){
-    console.log('Clicked!!')
-    console.log('input', txtInput.value)
+function clickEventHandler() {
+    // console.log('Clicked!!')
+    // console.log('input', txtInput.value)
+    outputDiv.innerText = 'akfjdnf '+txtInput.value
 }
 
-btnTranslater.addEventListener('click',clickEventHandler )
+btnTranslater.addEventListener('click', clickEventHandler)
